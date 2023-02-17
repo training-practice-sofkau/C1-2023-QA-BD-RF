26e3c7a8efbffc38265c93e170a019f5cfbce985
MiguelMendoza22 <jesusmiguelmm@ufps.edu.co>
2023-02-17 09:40:38 -0500

    se actualiza el script tienda agregandole 4 procedimientos

--- script tienda.sql
+++ script tienda.sql
@@ -141,7 +141,7 @@ INNER JOIN pedido ON producto_pedido.id_pedido_producto = pedido.id_pedido
 INNER JOIN producto ON producto_pedido.id_producto_pedido = producto.id_producto
 GROUP BY pedido.id_pedido;
 
--- 5 cosulta para ver los clientes con pedidos mayores a 60000
+-- 5 consulta para ver los clientes con pedidos mayores a 60000
 SELECT cliente.nombre_cliente, cliente.cedula, pedido.total_valor
 FROM cliente 
 INNER JOIN pedido ON cliente.cedula = pedido.cedula_cliente 
@@ -155,7 +155,7 @@ WHERE zona_domiciliario = 'Zona 2';
 -- 7 consulta para ver los domiciliarios que hay en cada zona de cada cliente
 SELECT cliente.nombre_cliente, cliente.zona_cliente, domiciliario.nombre_domiciliario, domiciliario.zona_domiciliario
 FROM cliente
-JOIN domiciliario ON cliente.zona_cliente = domiciliario.zona_domiciliario;
+INNER JOIN domiciliario ON cliente.zona_cliente = domiciliario.zona_domiciliario;
 
 -- 8 consulta para ver los productos con sus stock
 SELECT producto.nombre_producto, producto.stock
@@ -167,7 +167,7 @@ FROM domiciliario
 INNER JOIN pedido ON pedido.id_domiciliario_pedido = domiciliario.id_domiciliario;
 
 -- 10 consulta para ver los clientes y las id de los pedidos que han realizado
-SELECT cliente.nombre_cliente, GROUP_CONCAT(pedido.id_pedido SEPARATOR ', ') as pedidos_realizados
+SELECT 	 GROUP_CONCAT(pedido.id_pedido SEPARATOR ', ') as pedidos_realizados
 FROM cliente
 LEFT JOIN pedido ON cliente.cedula = pedido.cedula_cliente
 GROUP BY cliente.cedula;
@@ -213,4 +213,76 @@ GROUP BY pedido.id_pedido;
 SELECT *
 FROM info_pedido;
 
+-- procedimiento para agregar un telefono al cliente
+DELIMITER //
+CREATE PROCEDURE agregar_telefono_cliente(
+  IN cedula_procedimiento VARCHAR(15),
+  IN numero_procedimiento VARCHAR(15)
+)
+BEGIN
+  INSERT INTO telefono_cliente(cedula_cliente, numero_cliente)
+  VALUES(cedula_procedimiento, numero_procedimiento);
+END//
+// DELIMITER;
+
+CALL agregar_telefono_cliente('123456789', '555-5555')
+
+SELECT *
+FROM telefono_cliente
+
+-- procedimiento para actualizar el telefono de un cliente
+DELIMITER //
+CREATE PROCEDURE actualizar_telefono_cliente (
+IN cedula_procedimiento VARCHAR(15),
+IN numero_procedimiento VARCHAR(15),
+IN nuevo_numero_procedimiento VARCHAR(15)
+)
+BEGIN
+UPDATE telefono_cliente
+SET numero_cliente = nuevo_numero_procedimiento
+WHERE cedula_cliente = cedula_procedimiento AND numero_cliente = numero_procedimiento;
+END //
+//DELIMITER ;
+
+CALL actualizar_telefono_cliente ('123456789','555-5555','666-6666')
+
+-- Procedimiento para consultar numero de telefonos del cliente 
+DELIMITER // 
+CREATE PROCEDURE consultar_telefono_cliente (
+IN cedula_procedimiento VARCHAR(15)
+)
+BEGIN 
+SELECT numero_cliente
+FROM telefono_cliente
+WHERE cedula_cliente = cedula_procedimiento;
+END //
+//DELIMITER ;
+
+CALL consultar_telefono_cliente ('123456789')
+
+-- Procedimiento para eliminar numero de telefonos del cliente 
+DELIMITER //
+CREATE PROCEDURE eliminar_telefono_cliente (
+IN cedula_procedimiento VARCHAR(10),
+IN numero_procedimiento VARCHAR(15)
+)
+BEGIN
+DELETE FROM telefono_cliente
+WHERE cedula_cliente = cedula_procedimiento AND numero_cliente = numero_procedimiento;
+END //
+//DELIMITER ;
+
+CALL eliminar_telefono_cliente ('123456789','666-6666')
+
+DELIMITER //
+CREATE TRIGGER disminuir_stock AFTER INSERT ON producto_pedido 
+FOR EACH ROW
+BEGIN
+  IF NEW.Cantidad > (SELECT stock FROM producto WHERE id_producto = NEW.id_producto_pedido) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No hay suficiente stock para realizar el pedido';
+  ELSE
+    UPDATE producto SET stock = stock - NEW.Cantidad WHERE id_producto = NEW.id_producto_pedido;
+  END IF;
+END;
+// DELIMITER ;
 
