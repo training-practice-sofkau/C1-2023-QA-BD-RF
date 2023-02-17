8a1063c1d198a272b64c2707295128bb606d23b7
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-02-17 11:55:23 -0500

    Registros de tablas mediante models y repositorys en java

---
+++ Conexion-Barberia-mysql/src/main/java/com/sofkau/mysql/model/Cita.java
@@ -0,0 +1,46 @@
+package com.sofkau.mysql.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Cita {
+    private int id;
+    private int cliente;
+    private int empleado;
+    private Date fechaHora;
+
+
+
+    public Cita() {
+    }
+
+    public Cita(int cliente, int empleado, Date fechaHora) {
+        this.cliente = cliente;
+        this.empleado = empleado;
+        this.fechaHora = fechaHora;
+    }
+
+    public int getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(int cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(int empleado) {
+        this.empleado = empleado;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+}
---
+++ Conexion-Barberia-mysql/src/main/java/com/sofkau/mysql/model/Producto.java
@@ -0,0 +1,60 @@
+package com.sofkau.mysql.model;
+
+public class Producto {
+    private Long ref;
+    private String nombre;
+    private Integer cantidad;
+    private Double precio;
+    private String proveedor;
+
+    public Producto(Long ref, String nombre, Integer cantidad, Double precio, String proveedor) {
+        this.ref = ref;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.proveedor = proveedor;
+    }
+
+    public Producto() {
+    }
+
+    public Long getRef() {
+        return ref;
+    }
+
+    public void setRef(Long ref) {
+        this.ref = ref;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+}
