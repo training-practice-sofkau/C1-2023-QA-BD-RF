b20c9017fb00ddcadd271da528c04cb58bb732d6
Ococho <jonathan.vargas85@gmail.com>
2023-02-17 11:07:01 -0500

    ✨ Adding SQL script with queries to view data

---
+++ BD/SQLScripts/DataQueries.sql
@@ -0,0 +1,91 @@
+-- -----------------------------------------------------
+-- Sentencias de visualización de la información
+-- -----------------------------------------------------
+USE ocochosBarberia;
+
+-- -----------------------------------------------------
+-- Visualizar stock de productos
+-- -----------------------------------------------------
+SELECT
+		REFProducto AS 'Referencia',
+        nombreProducto AS 'Producto',
+        cantidadProducto AS 'Unidades',
+        precioProducto AS 'Precio'
+FROM producto;
+
+-- -----------------------------------------------------
+-- Visualizar stock de insumos
+-- -----------------------------------------------------
+SELECT
+		REFInsumo AS 'Referencia',
+        nombreInsumo AS 'Producto',
+        cantidadProducto AS 'Unidades'
+FROM insumo;
+
+-- -----------------------------------------------------
+-- Visualizar servicios realizados
+-- -----------------------------------------------------
+SELECT
+		idServicio AS 'Servicio N°',
+        duración AS 'Duración',
+        fechaServicio AS 'Realizado en',
+        descripción AS 'Descripción'
+FROM servicio;
+
+-- -----------------------------------------------------
+-- Visualizar empleados en nomina
+-- -----------------------------------------------------
+SELECT
+		nombreEmpleado AS 'Empleado',
+        cédula AS 'Identificación'
+FROM empleado;
+
+-- -----------------------------------------------------
+-- Visualizar clientes registrados
+-- -----------------------------------------------------
+SELECT
+		nombreCliente AS 'Cliente',
+        correo AS 'Correo',
+        teléfonoCliente AS 'Teléfono'
+FROM cliente;
+
+-- -----------------------------------------------------
+-- Visualizar proveedores de productos e insumos
+-- -----------------------------------------------------
+SELECT
+		nombreProveedor AS 'Proveedor',
+        teléfonoProveedor AS 'Teléfono'
+FROM proveedor;
+
+-- -----------------------------------------------------
+-- Visualizar procedimientos ofrecidos actualmente
+-- -----------------------------------------------------
+SELECT
+		tipo AS 'Ofrecemos',
+        precioProcedimiento AS 'Precio'
+FROM procedimiento;
+
+-- -----------------------------------------------------
+-- Visualizar la cantidad de ventas realizadas
+-- -----------------------------------------------------
+SELECT COUNT(idVenta) AS 'Cantidad de ventas realizadas'
+FROM venta;
+
+-- -----------------------------------------------------
+-- Visualizar todas citas agendadas
+-- -----------------------------------------------------
+SELECT
+		idCita AS 'Cita N°',
+        fechaCita AS 'Agendada para el',
+        c.nombreCliente AS 'Cliente'
+FROM cita, cliente AS c
+WHERE idClienteC = c.idCliente;
+
+-- -----------------------------------------------------
+-- Visualizar los empleados y sus citas asignar
+-- -----------------------------------------------------
+SELECT nombreEmpleado AS 'Empleado', 
+GROUP_CONCAT(c.idCita SEPARATOR ' | ') AS 'Cita(s)'
+FROM empleado
+INNER JOIN cita AS c ON idEmpleado = c.idEmpleadoC  
+GROUP BY idEmpleado;
