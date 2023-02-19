b8524727bbbcadcb9681045e25ee736c5a96c707
Proximacentaurion <cristiancvn63@gmail.com>
2023-02-19 03:48:19 -0500

    Adición de la segunda vista para la solución
    Adición de la segunda vista para la solución

--- 10 Sentencias SQL para creacion de vistas.sql
+++ 10 Sentencias SQL para creacion de vistas.sql
@@ -8,3 +8,11 @@ LEFT JOIN responsable r ON p.cedula_res = r.cedula_res
 LEFT JOIN tel_responsable t ON r.cedula_res = t.cedula_res
 LEFT JOIN domiciliario d ON p.cedula_dom = d.cedula_dom
 LEFT JOIN tel_domiciliario td ON d.cedula_dom = td.cedula_dom;
+
+
+CREATE VIEW vista_cli_cesta AS
+SELECT c.cedula_cli, c.primer_nombre, pr.nombre_prod, ce.cantidad
+FROM cliente c
+LEFT JOIN pedido p ON c.ID_cli = p.ID_cli
+LEFT JOIN cesta ce ON p.codigo_pedido = ce.codigo_pedido
+LEFT JOIN producto pr ON ce.ID_producto = pr.ID_producto;
