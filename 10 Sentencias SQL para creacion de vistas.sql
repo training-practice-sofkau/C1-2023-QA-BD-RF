83aa3dbf329ce72aef6c8653bd806c3af486cc20
Proximacentaurion <cristiancvn63@gmail.com>
2023-02-19 03:22:36 -0500

    Se crea la primer vista de la solución
    Se crea la primer vista de la solución

---
+++ 10 Sentencias SQL para creacion de vistas.sql
@@ -0,0 +1,10 @@
+CREATE VIEW vista_clientespedido AS
+SELECT c.cedula_cli, c.primer_nombre, p.codigo_pedido, p.estado, 
+r.primer_nombre AS responsable_nombre, t.telefono_res AS responsable_telefono, 
+d.primer_nombre AS domiciliario_nombre, td.telefono_dom AS domiciliario_telefono
+FROM cliente c
+LEFT JOIN pedido p ON c.ID_cli = p.ID_cli
+LEFT JOIN responsable r ON p.cedula_res = r.cedula_res
+LEFT JOIN tel_responsable t ON r.cedula_res = t.cedula_res
+LEFT JOIN domiciliario d ON p.cedula_dom = d.cedula_dom
+LEFT JOIN tel_domiciliario td ON d.cedula_dom = td.cedula_dom;
